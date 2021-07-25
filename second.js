10023359db098cbb852d4bf8b445be6c3eb95849
vivaan120 <83453732+vivaan120@users.noreply.github.com>
2021-07-25 21:12:34 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,8 @@
+
+function addUser(){
+    player1_name=document.getElementById("player1_name_input").value;
+    player2_name=document.getElementById("player2_name_input").value;
+    localStorage.setItem("player 1 name",player1_name);
+    localStorage.setItem("player 2 name",player2_name);
+    window.location="second.html";
+}
